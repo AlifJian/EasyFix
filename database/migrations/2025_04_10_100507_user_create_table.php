e4ceff36f63452ca4b27d38fee50c93f961c2116
Alif Jian <alifjian834@gmail.com>
2025-04-10 17:51:36 +0700

    Add : User Migrations

---
+++ database/migrations/2025_04_10_100507_user_create_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use GuzzleHttp\Promise\Create;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('users', function (Blueprint $table) {
+        
+            $table->id();
+            $table->string("name");
+            $table->string("email")->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string("phone_number");
+            $table->string("password");
+            $table->string("address");
+            $table->enum("role", ["admin","user"])->default("user");
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
