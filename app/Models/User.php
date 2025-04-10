9891cfda8317dfbc1c5e48c7a2c65017c9544141
Alif Jian <alifjian834@gmail.com>
2025-04-10 18:33:52 +0700

    Add : User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+
+class User extends Authenticatable
 {
     //
     use SoftDeletes;
+
+
+    protected $fillable = [
+        "name",
+        "email",
+        "password",
+        "phone_number",
+        "address"
+    ];
+
+    protected $hidden = [
+        "password"
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
